b4f7de68e407ca77af67774873ddee1bcab1efb2
podsicles <jessicaflores.business@gmail.com>
2023-07-20 15:41:24 +0800

    predefine 3 categories in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = ['COVID', 'Earthquake', 'Requirements']
+
+categories.each do |category|
+  category = Category.find_or_create_by(name: category) 
+end
