baef132492102cdbfe5d6dfe8c0ae4f75a4d9c13
podsicles <jessicaflores.business@gmail.com>
2023-07-20 15:01:05 +0800

    done post category ships

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   validates :content, presence: true
 
   has_many :comments
-  has_many :categories
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
 
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_033738) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_063903) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_033738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "post_category_ships", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_category_ships_on_category_id"
+    t.index ["post_id"], name: "index_post_category_ships_on_post_id"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
