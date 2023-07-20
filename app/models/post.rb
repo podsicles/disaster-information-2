394391e332e838b671b6c127f762165860dd4242
podsicles <jessicaflores.business@gmail.com>
2023-07-20 15:08:08 +0800

    add soft kill

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,16 @@
 class Post < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
+
   validates :title, presence: true
   validates :content, presence: true
 
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  def destroy
+    update(deleted_at: Time.now)
+  end
 
   belongs_to :user
 end
