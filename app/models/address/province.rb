365767bef291f7138ee912b6cf859950253e2d2d
podsicles <jessicaflores.business@gmail.com>
2023-07-20 16:10:26 +0800

    create model Address::Province

---
+++ app/models/address/province.rb
@@ -0,0 +1,6 @@
+class Address::Province < ApplicationRecord
+  validates :name, presence: true
+  validates :code, uniqueness: true
+  
+  belongs_to :region 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_075021) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_080745) do
+  create_table "address_provinces", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "region_id"
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_address_provinces_on_region_id"
+  end
+
   create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
     t.string "code"
     t.string "name"
