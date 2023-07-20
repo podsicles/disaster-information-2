baf93d9a8b48834d50c82531de7afec8f63b546f
podsicles <jessicaflores.business@gmail.com>
2023-07-20 17:25:46 +0800

    add web API design

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,22 @@ Rails.application.routes.draw do
     resources :comments, except: :show
   end
   resources :categories, except: :show
+
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+  
+        resources :provinces, only: %i[index show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+      
+      resources :cities, only: %i[index show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+      
+      resources :barangays, only: %i[index show], defaults: { format: :json }
+    end
+  end
 end
