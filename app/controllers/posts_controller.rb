affb1dfb390ab4a97cfbeec0aa2b65fb8627a25a
podsicles <jessicaflores.business@gmail.com>
2023-07-20 09:16:16 +0800

    add authentication for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).all
   end
 
   def new
@@ -11,6 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       flash[:notice] = 'Post created successfully'
       redirect_to posts_path
@@ -43,6 +46,13 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def validate_post_owner
+    unless @post.user == current_user
+      flash[:notice] = 'This post does not belong to you'
+      redirect_to posts_path
+    end
+  end
 
   def set_post
     @post = Post.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_231943) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_005146) do
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
