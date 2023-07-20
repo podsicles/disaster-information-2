24bb38e9fe6f83fed3192f965cdffcfd55ceed0f
podsicles <jessicaflores.business@gmail.com>
2023-07-20 10:02:49 +0800

    create model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_005146) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_011842) do
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -32,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_005146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
