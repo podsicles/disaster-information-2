81e7f8a814b4d4def83bc828ae3c8788ca505e1a
podsicles <jessicaflores.business@gmail.com>
2023-07-21 17:03:48 +0800

    add pagination to posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:categories, :user, :region, :province).all
+    @posts = Post.includes(:categories, :user, :region, :province).page(params[:page]).per(5)
   end
 
   def new
