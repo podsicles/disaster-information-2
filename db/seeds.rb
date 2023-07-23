c96723ae62215ef3a58fdb8745abc4c79f10b180
podsicles <jessicaflores.business@gmail.com>
2023-07-23 17:00:24 +0800

    add seeds to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
+  puts "create user id: #{user.id}, email: #{user.email}"
+end
 
 categories = ['COVID', 'Earthquake', 'Requirements']
 
 categories.each do |category|
   category = Category.find_or_create_by(name: category) 
+end
+
+30.times do |i|
+  puts "start create #{i} post"
+  region = Address::Region.find_by_name("National Capital Region")
+  province = region.provinces.find_by_name("Fourth District")
+  city = province.cities.find_by_name("City of Makati")
+  barangay = city.barangays.sample
+
+  post = Post.create(
+    title: Faker::Lorem.sentence, 
+    content: Faker::Lorem.paragraph, 
+    user: User.all.sample, 
+    categories: Category.all.sample((1..3).to_a.sample), 
+    address: Faker::Address.street_address, 
+    region: region,
+    province: province,
+    city: city,
+    barangay: barangay,
+    ip_address: Faker::Internet.public_ip_v4_address
+  )
+  (1..100).to_a.sample.times do
+    Comment.create(content: Faker::Lorem.sentence, user: User.all.sample, post: post)
+  end
+  puts "finish #{i} post"
 end
